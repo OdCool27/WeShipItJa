95129cea9d0eb5da7b1a84a523af4a9e8139bdc9
OdCool27 <odanecollins@gmail.com>
2025-10-17 13:17:53 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Hamburger
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navbar.classList.toggle('active');
+});
+
+
+
